171b7bb0b85e32284f71922033c803505248f348
Letícia Dummer <62682118+letdummer@users.noreply.github.com>
2024-10-08 16:14:57 +0100

    Create ft_ft.c

---
+++ c_01/ex00/ft_ft.c
@@ -0,0 +1,18 @@
+/*#include <stdio.h>
+
+void	ft_ft(int *nbr);
+
+int	main(void)
+{
+	int	n;
+	int	*nbr;
+
+	nbr = &n;
+	ft_ft(nbr);
+	printf("%d", n);
+}
+*/
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
