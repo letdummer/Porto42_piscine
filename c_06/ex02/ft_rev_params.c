d96bb93d3453e07cf36984ac8f0d7445a66a9883
Letícia Dummer <62682118+letdummer@users.noreply.github.com>
2024-10-08 17:15:36 +0100

    Create ft_rev_params.c

---
+++ c_06/ex02/ft_rev_params.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+
+	i = argc - 1;
+	while (i > 0)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		i--;
+		ft_putchar('\n');
+	}
+}
