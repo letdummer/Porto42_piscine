b110f77061c60821d3c86f545cc283efa40c2a42
Letícia Dummer <62682118+letdummer@users.noreply.github.com>
2024-10-08 16:16:10 +0100

    Create ft_ultimate_ft.c

---
+++ c_01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,34 @@
+/*#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr);
+
+int	main(void)
+{
+	int	n;
+	int	*ptr_n1;
+	int	**ptr_n2;
+	int	***ptr_n3;
+	int	****ptr_n4;
+	int	*****ptr_n5;
+	int	******ptr_n6;
+	int	*******ptr_n7;
+	int	********ptr_n8;
+	int	*********ptr_n9;
+
+	ptr_n1 = &n;
+	ptr_n2 = &ptr_n1;
+	ptr_n3 = &ptr_n2;
+	ptr_n4 = &ptr_n3;
+	ptr_n5 = &ptr_n4;
+	ptr_n6 = &ptr_n5;
+	ptr_n7 = &ptr_n6;
+	ptr_n8 = &ptr_n7;
+	ptr_n9 = &ptr_n8;
+	ft_ultimate_ft(ptr_n9);
+	printf("%d", n);
+}
+*/
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
