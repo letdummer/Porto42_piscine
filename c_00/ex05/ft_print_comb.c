701eb36823515fbcc56c30bb77cbd5aa80e270f6
Letícia Dummer <62682118+letdummer@users.noreply.github.com>
2024-10-08 15:51:44 +0100

    Create ft_print_comb.c

---
+++ c_00/ex05/ft_print_comb.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+
+/*void	ft_print_comb(void);
+
+int	main(void)
+{
+	ft_print_comb();
+}
+*/
+void	ft_putchar(char x)
+{
+	write(1, &x, 1)
+}
+
+void	ft_print_comb(void)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = '0';
+
+	while (a <= '7')
+	{
+		b = a + 1;
+
+		while (b <= '8')
+		{
+			c = b + 1;
+
+			while (c <= '9')
+			{	
+				ft_putchar(a + 1);
+				ft_putchar(b + 1);
+				ft_putchar(c + 2);
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+		}
+	}
+}
